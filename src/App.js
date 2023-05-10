f87c8a0c6912b4cf723e7fd3bac2c6843cf7976a
Naramatsu <joncyy_19@hotmail.com>
2023-05-09 21:25:02 -0500

    added a list

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ function App() {
       <h1>Hello from LS</h1>
       <p>El Boss es el Jona</p>
       <p>Estoy feliz y corroncho por programar</p>
+      <p>Empezando las Learning sessions</p>
+      <ul>
+        <li>Teacher: Jonathan</li>
+        <li>Student: Dairo</li>
+        <li>Student: Edwin</li>
+      </ul>
     </div>
   );
 }
